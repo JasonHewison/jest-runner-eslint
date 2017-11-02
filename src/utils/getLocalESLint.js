b5bc4268b9027e268f0613c38a77bc7ecbb3fdd5
Jason <JasonHewison@users.noreply.github.com>
2017-11-02 16:25:46 +0000

    Added overrideNodeModulesPath config option
    overrideNodeModulesPath allows you to change the nodeModulesPath that eslint will be loaded from. This is needed to workaround an issue using jest-runner-eslint within a yarn workspace. When using Yarn Workspaces the node modules are hoisted into the parent project's node_modules folder so eslint cannot be found in the "rootDir".

--- src/utils/getLocalESLint.js
+++ src/utils/getLocalESLint.js
@@ -2,7 +2,7 @@ const path = require('path');
 const findUp = require('find-up');
 
 const getLocalESLint = config => {
-  const nodeModulesPath = findUp.sync('node_modules', { cwd: config.rootDir });
+  const nodeModulesPath = config.overrideNodeModulesPath || findUp.sync('node_modules', { cwd: config.rootDir });
   // eslint-disable-next-line import/no-dynamic-require, global-require
   return require(path.resolve(nodeModulesPath, 'eslint'));
 };
