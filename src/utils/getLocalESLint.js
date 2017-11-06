26d37a7fbbb531b44a509f94214e67daa74a2f18
Jason <JasonHewison@users.noreply.github.com>
2017-11-06 10:14:44 +0000

    Changed to use a global variable
    Changed getLocalESLint.js to use the JEST_RUNNER_ESLINT_OVERRIDE_NODE_MODULES_PATH global variable.

--- src/utils/getLocalESLint.js
+++ src/utils/getLocalESLint.js
@@ -2,7 +2,7 @@ const path = require('path');
 const findUp = require('find-up');
 
 const getLocalESLint = config => {
-  const nodeModulesPath = config.overrideNodeModulesPath || findUp.sync('node_modules', { cwd: config.rootDir });
+  const nodeModulesPath = config.globals.JEST_RUNNER_ESLINT_OVERRIDE_NODE_MODULES_PATH || findUp.sync('node_modules', { cwd: config.rootDir });
   // eslint-disable-next-line import/no-dynamic-require, global-require
   return require(path.resolve(nodeModulesPath, 'eslint'));
 };
